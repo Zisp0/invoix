a55414b0a8ddc08c349b1e1c5a93734292b53654
Zisp0 <ct738751@gmail.com>
2025-05-14 23:17:46 -0500

    eliminar factura

--- frontend/src/app/components/invoice-list/invoice-list.component.ts
+++ frontend/src/app/components/invoice-list/invoice-list.component.ts
@@ -3,6 +3,7 @@ import { InvoiceService } from '../../services/invoice.service';
 import { Invoice } from '../../models/invoice.model';
 import { CommonModule, NgForOf } from '@angular/common';
 import { Router, RouterModule } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-invoice-list',
@@ -30,7 +31,23 @@ export class InvoiceListComponent {
   }
 
   
-  navigateToNew() {
-    this.router.navigate(['/invoices/create']);
+  deleteInvoice(id: number) {
+    Swal.fire({
+    title: '¿Estás seguro?',
+    text: `Esto eliminará la factura #${id}.`,
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#d33',
+    cancelButtonColor: '#3085d6',
+    confirmButtonText: 'Sí, eliminar',
+    cancelButtonText: 'Cancelar'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      this.invoiceService.deleteInvoice(id).subscribe(() => {
+        this.invoices = this.invoices.filter(inv => inv.id !== id);
+        Swal.fire('Eliminado', 'La factura fue eliminada correctamente.', 'success');
+      });
+    }
+  });
   }
 }
